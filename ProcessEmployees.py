77a716f65c60ea589218a5dedda592fe7c0efcca
BhojwaniUnderling1441 <123410398+BhojwaniUnderling1441@users.noreply.github.com>
2023-02-13 13:30:44 -0600

    Finished Quiz I

--- ProcessEmployees.py
+++ ProcessEmployees.py
@@ -11,18 +11,25 @@ Iternate through the dictionary to print out their name and thier new salary (as
 import csv
 
 #open the file
-
-
-
+infile = open('employee_data.csv','r',newline = '')
+reader = csv.reader(infile)
+next(reader)
 
 #create an empty dictionary
-
+employee_dict = {}
 
 #use a loop to iterate through the csv file
-
-
+for row in reader:
     #check if the employee fits the search criteria
+    if(row[3] == 'Marketing' and row[4] == 'CSR'):
+        name = row[1] + ' ' + row[2]
+        current_salary = format(float(row[5]),',.2f')
+        new_salary = float(row[5]) * 1.1
+        employee_dict[name] = format(new_salary,',.2f')
 
+        print(f"Manager Name: {name} Current Salary: ${current_salary}")
+
+infile.close()
 
     
 
@@ -32,7 +39,8 @@ print()
 
 #iternate through the dictionary and print out the key and value as per printout
 
-
+for key in employee_dict:
+    print(f"Manager Name: {key} New Salary: ${employee_dict[key]}")
 
           
         
